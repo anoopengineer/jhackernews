276c36000d2d3ce336aca11275696cd0a4533ecd
anoopengineer <anoopengineer@gmail.com>
2010-09-17 23:04:32 +0530

    added functionality to add comments

--- src/main/java/com/dailygyan/jhackernews/comment/HackerComments.java
+++ src/main/java/com/dailygyan/jhackernews/comment/HackerComments.java
@@ -2,10 +2,14 @@ package com.dailygyan.jhackernews.comment;
 
 import java.util.List;
 
+import com.dailygyan.jhackernews.user.AuthToken;
+
 public interface HackerComments {
 
     public List<Comment> getComments(String id);
 
     public List<Comment> getCommentsSubmittedBy(String userName);
+    
+    public boolean postComment(String userName, AuthToken token, Long id, String comment);
 
 }
--- src/main/java/com/dailygyan/jhackernews/comment/impl/HackerCommentsImpl.java
+++ src/main/java/com/dailygyan/jhackernews/comment/impl/HackerCommentsImpl.java
@@ -17,12 +17,17 @@ package com.dailygyan.jhackernews.comment.impl;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.List;
 
 import com.dailygyan.jhackernews.comment.Comment;
 import com.dailygyan.jhackernews.comment.HackerComments;
+import com.dailygyan.jhackernews.user.AuthToken;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -41,24 +46,145 @@ public class HackerCommentsImpl implements HackerComments {
         gson = new Gson();
     }
 
-    /* (non-Javadoc)
-     * @see com.dailygyan.jhackernews.comment.HackerComments#getComments(java.lang.String)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.dailygyan.jhackernews.comment.HackerComments#getComments(java.lang
+     * .String)
      */
     public List<Comment> getComments(String id) {
         return fetch(" http://api.ihackernews.com/comments/" + id);
     }
 
-    /* (non-Javadoc)
-     * @see com.dailygyan.jhackernews.comment.HackerComments#getCommentsSubmittedBy(java.lang.String)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.dailygyan.jhackernews.comment.HackerComments#getCommentsSubmittedBy
+     * (java.lang.String)
      */
     public List<Comment> getCommentsSubmittedBy(String userName) {
         return fetch("http://api.ihackernews.com/threads/" + userName);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.dailygyan.jhackernews.comment.HackerComments#postComment(java.lang
+     * .String, com.dailygyan.jhackernews.user.AuthToken, java.lang.Long,
+     * java.lang.String)
+     */
+    public boolean postComment(String userName, AuthToken token, Long id,
+            String comment) {
+        if (userName == null || token == null || id == null || comment == null) {
+            throw new IllegalArgumentException(
+                    "Arguments to postComment method cannot be null");
+        }
+
+        String data = createData(userName, token, id, comment);
+        return post(data);
+    }
+
+    /**
+     * Creates the data.
+     * 
+     * @param userName
+     *            the user name
+     * @param token
+     *            the token
+     * @param id
+     *            the id
+     * @param comment
+     *            the comment
+     * @return the string
+     */
+    private String createData(String userName, AuthToken token, Long id,
+            String comment) {
+        String data = null;
+        try {
+            data = URLEncoder.encode("username", "UTF-8") + "="
+                    + URLEncoder.encode(userName, "UTF-8");
+            data += "&" + URLEncoder.encode("token", "UTF-8") + "="
+                    + URLEncoder.encode(token.getToken(), "UTF-8");
+            data += "&" + URLEncoder.encode("id", "UTF-8") + "="
+                    + URLEncoder.encode(id.toString(), "UTF-8");
+
+            data += "&" + URLEncoder.encode("comment", "UTF-8") + "="
+                    + URLEncoder.encode(comment, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            data = null;
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    /**
+     * Post.
+     * 
+     * @param data
+     *            the data
+     * @return true, if successful
+     */
+    private boolean post(String data) {
+        if (null == data) {
+            return false;
+        }
+
+        boolean retVal = false;
+        HttpURLConnection conn = null;
+        try {
+            URL url = new URL("http://api.ihackernews.com/comment");
+
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type",
+                    "application/x-www-form-urlencoded");
+
+            conn.setRequestProperty("Content-Length", ""
+                    + Integer.toString(data.getBytes().length));
+            conn.setRequestProperty("Content-Language", "en-US");
+
+            conn.setUseCaches(false);
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn
+                    .getOutputStream());
+            wr.write(data);
+            wr.flush();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn
+                    .getInputStream()));
+            StringBuffer buffer = new StringBuffer();
+            String inputLine = null;
+
+            while ((inputLine = in.readLine()) != null) {
+                buffer.append(inputLine);
+            }
+
+            if ("true".equalsIgnoreCase(buffer.toString())) {
+                retVal = true;
+            } else {
+                retVal = false;
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+
+        return retVal;
+    }
+
     /**
      * Fetch.
      * 
-     * @param urlString the url string
+     * @param urlString
+     *            the url string
      * @return the list
      */
     private List<Comment> fetch(String urlString) {
